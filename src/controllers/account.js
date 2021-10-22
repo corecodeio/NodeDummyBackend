517a2a2d856e67073857c61897a51cfbbd7dcda9
Yosef Maldonado Arriaga <yosefmarr@gmail.com>
2021-10-21 18:53:55 -0600

    finishing dummy data example

--- src/controllers/account.js
+++ src/controllers/account.js
@@ -1,17 +1,17 @@
-const DUMMY = require('../util/dummy');
+const Account = require('../models/account');
 
 module.exports.createAccount = (req, res, next) => {
   const { name, type, currency } = req.body;
-  const account = DUMMY[DUMMY.length-1].account + 1;
-  DUMMY.push({ account, name, type, currency });
+  Account.create([{ account : 0, name, type, currency }]);
   res.status(200).json({ valid: true, message: 'Ok' });
 }
 
 module.exports.getAccount = (req, res, next) => {
-  const account = DUMMY.filter((a) => a.account === Number(req.params.id));
-  res.status(200).json({ valid: true, message: 'Ok', data: account });
+  const data = Account.findById([Number(req.params.id)]);
+  res.status(200).json({ valid: true, message: 'Ok', data });
 }
 
 module.exports.getAccounts = (req, res, next) => {
-  res.status(200).json({ valid: true, message: 'Ok', data: DUMMY });
+  const data = Account.fetchAll();
+  res.status(200).json({ valid: true, message: 'Ok', data });
 }
--- src/models/account.js
+++ src/models/account.js
@@ -1,1 +1,21 @@
-// TODO
+const DUMMY = require('../util/dummy');
+
+const Account = {};
+
+Account.create = (args) => {
+  const bindings = args[0];
+  const account = DUMMY[DUMMY.length-1].account + 1;
+  bindings.account = account;
+  DUMMY.push(bindings);
+}
+
+Account.findById = (args) => {
+  const id = args[0];
+  return DUMMY.filter((a) => a.account === id);
+}
+
+Account.fetchAll = () => {
+  return DUMMY;
+}
+
+module.exports = Account;
